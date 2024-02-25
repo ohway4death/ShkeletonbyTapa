c1f5038aac3480b0bb79862bd57e296bc23d33c5
ohway4death <ohway.pretz411@gmail.com>
2024-02-25 21:55:05 +0900

    sound_umekomi

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,16 @@
 #include <LovyanGFX.hpp>
 #include <LGFX_AUTODETECT.hpp>
 
+#include <driver/i2s.h>
+#include <WiFi.h>
+
+#include "AudioFileSourceSD.h"
+#include "AudioFileSourceID3.h"
+#include "AudioGeneratorMP3.h"
+#include "AudioOutputI2S.h"
+#include "wav1.h"
+#include "driver/i2s.h"
+
 /**RFID**/
 MFRC522_I2C mfrc522(0x28, -1); // Create MFRC522 instance.
 
@@ -27,7 +37,7 @@ int littleFingerLine[5] = {18, 19, 20};
 bool isCard = false; // カード読み取りフラグ
 #define ace "155 43 9 12"
 int CardID = 0;
-float TotalTime = 5; // 演出合計時間
+float TotalTime = 5000; // 演出合計時間(ms)
 unsigned long startMillis = 0;
 unsigned long currentMillis = 0;
 unsigned long previousLEDTime = 0;
@@ -49,7 +59,7 @@ void wait_display();
 #define jack "1d 75 01 55"
 #define queen "1d fd 6e 58"
 #define king "1d e1 27 55"
-void rotate_display(float diff);
+void rotate_display(float diff, int cardId);
 
 // 加速度リセット
 float baseAccX, baseAccY, baseAccZ = 0; // 基準加速度格納用
@@ -59,14 +69,31 @@ float maxAccX, maxAccY, maxAccZ = 0;
 bool reset_flg = false;
 void check_acc();
 void zeroSet();
-void shakeReset();
+boolean shakeReset();
 
+// 効果音
+AudioGeneratorMP3 *mp3;
+AudioFileSourceSD *file;
+AudioOutputI2S *out;
+AudioFileSourceID3 *id3;
+#define OUTPUT_GAIN 80
+bool se_flag = false;
+
+#define SPEAKER_I2S_NUMBER      I2S_NUM_0
+const unsigned char *wavList[] = {wav1};
+const size_t wavSize[] = {sizeof(wav1)};
+
+// プロトタイプ宣言
 void Fingertip2Wrist(int ledPosition, int ledBrightness);
 bool isNewCard();
 int identifyCard();
 void LEDcontrol(int B, unsigned long C, unsigned long D);
 void LCDcontrol(int B, unsigned long C, unsigned long D);
 void uid_display_proc();
+void acc_setup();
+void sound_effect_setup();
+void SEcontrol();
+void InitI2SSpeakerOrMic(int mode);
 
 // ---------------------------------------------------------------
 void setup()
@@ -74,12 +101,15 @@ void setup()
   pixels.begin(); // NeoPixelの初期化
   pixels.clear(); // NeoPixelのリセット
 
-  Serial.begin(19200); // シリアル通信の開始
+  Serial.begin(115200); // シリアル通信の開始
 
   M5.begin();
   Wire.begin();
 
   wait_display_setup(); // 待機画面SCANのsetup
+  acc_setup(); // shake_resetのためのsetup
+  sound_effect_setup();
+
 }
 
 // ---------------------------------------------------------------
@@ -90,28 +120,29 @@ void loop()
   if (!isCard)
   {
     pixels.clear(); // NeoPixelのリセット
+    pixels.show();  // NeoPixelのリセット
     wait_display(); // 待機画面
     if (isNewCard())
     {
       CardID = identifyCard();
       // CardID = 2;
       isCard = true;
+      se_flag = true;
       startMillis = currentMillis;
     }
   }
   else
   {
     LEDcontrol(CardID, startMillis, currentMillis);
-    LCDcontrol(2, startMillis, currentMillis);
-    if ((currentMillis - startMillis) > 5000)
+    LCDcontrol(CardID, startMillis, currentMillis);
+    SEcontrol();
+    Serial.printf("startMillis = %d, currentMillis = %d, diff = %d \n", startMillis, currentMillis, (currentMillis - startMillis));
+
+    if ((currentMillis - startMillis) > TotalTime)
     {
-      // zeroSet();
-      // while (!reset_flg)
-      // {
-      //   shakeReset();
-      // }
+      zeroSet();
+      while (!shakeReset())
       isCard = false; // isCardをbooleanで更新
-      reset_flg = false;
     }
   }
 }
@@ -143,10 +174,10 @@ int identifyCard()
     }
   }
   String strUID = strBuf[0] + " " + strBuf[1] + " " + strBuf[2] + " " + strBuf[3];
+
+  // カードを増やしたい場合はdefine増やしてからここに追加
   if (strUID.equalsIgnoreCase(ace))
   {
-    // spriteImage.drawJpgFile(SD, "/trump/card_spade_01.jpg", 0, 0);
-    //  spriteImage.pushSprite(&lcd, x, y);
     return 1;
   }
 }
@@ -192,7 +223,10 @@ void LCDcontrol(int ID, unsigned long StartTime, unsigned long CurrentTime)
     // ledBrightness =100;
     // ledPosition =0;
   }
-  switch (ID)
+
+  int action = 2; // 演出を指定
+
+  switch (action)
   {
   case 1:
     if ((CurrentTime - previousLCDTime) > 100)
@@ -200,17 +234,6 @@ void LCDcontrol(int ID, unsigned long StartTime, unsigned long CurrentTime)
       previousLCDTime = CurrentTime;
     }
     // M5.Lcd.println(previousLEDTime);
-
-    /*
-      ledBrightness +=10;
-      ledPosition +=1;
-    if(ledPosition >= 5){
-      ledPosition = 0;
-    }
-    if(ledBrightness >= 250){
-      ledBrightness = 250;
-    }
-    */
     break;
 
   case 2:
@@ -218,7 +241,7 @@ void LCDcontrol(int ID, unsigned long StartTime, unsigned long CurrentTime)
     { // 100ms間隔で更新
       float diff = currentMillis - startMillis;
       previousLCDTime = CurrentTime;
-      rotate_display(diff);
+      rotate_display(diff, ID);
     }
   default:
     break;
@@ -315,67 +338,41 @@ void wait_display()
 // 回転演出
 int i = 0;
 int mode = 1;
-void rotate_display(float diff)
+void rotate_display(float diff, int cardId)
 {
-  // String strBuf[mfrc522.uid.size];
-  // for (byte i = 0; i < mfrc522.uid.size; i++)
-  // {
-  //   strBuf[i] = String(mfrc522.uid.uidByte[i], HEX);
-  //   if (strBuf[i].length() == 1)
-  //   {
-  //     strBuf[i] = "0" + strBuf[i];
-  //   }
-  // }
+  switch (cardId)
+  {
+  case 1:
+    spriteImage.drawJpgFile(SD, "/trump/card_spade_01.jpg", 0, 0);
+    break;
+  }
 
-  // String strUID = strBuf[0] + " " + strBuf[1] + " " + strBuf[2] + " " + strBuf[3];
+  float round_diff = round(diff / 100) * 100; // きっちり5000で回転が終わるように調整
 
-  // if (strUID.equalsIgnoreCase(ace))
-  // {
-  //   spriteImage.drawJpgFile(SD, "/trump/card_spade_01.jpg", 0, 0);
-  // }
-  // else if (strUID.equalsIgnoreCase(jack))
-  // {
-  //   spriteImage.drawJpgFile(SD, "/trump/card_spade_11.jpg", 0, 0);
-  // }
-  // else if (strUID.equalsIgnoreCase(queen))
-  // {
-  //   spriteImage.drawJpgFile(SD, "/trump/card_spade_12.jpg", 0, 0);
-  // }
-  // else if (strUID.equalsIgnoreCase(king))
-  // {
-  //   spriteImage.drawJpgFile(SD, "/trump/card_spade_13.jpg", 0, 0);
-  // }
-  // else
-  // {
-  //   Serial.println(strUID);
-  // }
-
-  // if (mode == 1)
-  // {
-  //   spriteImage.drawJpgFile(SD, "/trump/card_spade_01.jpg", 0, 0);
-  // }
-
-  // for (float i = 0; i <= 360; i += 10)
-  // {
-  //   spriteBase.fillScreen(BLACK); // 画面の塗りつぶし
-  //   spriteImage.pushRotateZoom(160, 120, i, i / 360, i / 360);
-  //   spriteBase.pushSprite(0, 0);
-  //   delay(100);
-  // }
-  spriteImage.drawJpgFile(SD, "/trump/card_spade_01.jpg", 0, 0);
   spriteBase.fillScreen(BLACK); // 画面の塗りつぶし
-  spriteImage.pushRotateZoom(160, 120, diff * 360 / 5000, diff * 1 / 5000, diff * 1 / 5000);
-  // spriteImage.pushRotateZoom(160, 120, 0, 1, 1);
+  spriteImage.pushRotateZoom(160, 120, round_diff * 360 / TotalTime, round_diff * 1 / TotalTime, round_diff * 1 / TotalTime);
   spriteBase.pushSprite(0, 0);
-  // i += 10;
-  // if (i > 360)
-  // {
-  //   i = 0;
-  // }
-  // delay(100);
 }
 // ---------------------------------------------------------------
-void shakeReset()
+// 加速度のセットアップ
+void acc_setup()
+{
+  M5.IMU.Init();                     // 6軸センサ初期化
+  M5.IMU.SetAccelFsr(M5.IMU.AFS_2G); // 加速度センサースケール初期値 ±2G(2,4,8,16)
+}
+// ---------------------------------------------------------------
+// 加速度リセット用
+void zeroSet()
+{
+  M5.update();
+  M5.IMU.getAccelData(&accX, &accY, &accZ); // 加速度データ取得
+  baseAccX = accX;                          // 取得値を補正値としてセット
+  baseAccY = accY;
+  baseAccZ = accZ;
+}
+// ---------------------------------------------------------------
+// 端末を振ってリセット画面へ
+boolean shakeReset()
 {
   M5.update();
   M5.IMU.getAccelData(&accX, &accY, &accZ); // 加速度データ取得
@@ -386,24 +383,86 @@ void shakeReset()
 
   if (diffAccY > 0.5)
   {
-    reset_flg = true;
+    return true;
+  }
+  else
+  {
+    return false;
   }
 }
 // ---------------------------------------------------------------
-// 加速度リセット用
-void zeroSet()
+// 効果音のセットアップ
+void sound_effect_setup()
 {
-  M5.IMU.getAccelData(&accX, &accY, &accZ); // 加速度データ取得
-  baseAccX = accX;                          // 取得値を補正値としてセット
-  baseAccY = accY;
-  baseAccZ = accZ;
+  M5.Axp.SetSpkEnable(true);
+  InitI2SSpeakerOrMic(MODE_SPK);
+
+  file = new AudioFileSourceSD("/mp3/drumroll.mp3");
+  id3 = new AudioFileSourceID3(file);
+  out = new AudioOutputI2S(0, 0); // Output to ExternalDAC
+  out->SetPinout(12, 0, 2);
+  out->SetOutputModeMono(true);
+  out->SetGain(0.3);
+  mp3 = new AudioGeneratorMP3();
 }
 // ---------------------------------------------------------------
-// 加速度取得用
-void check_acc()
+// 効果音コントロール
+void SEcontrol()
 {
-  zeroSet();
-  M5.update();                              // ボタン状態更新
-  M5.IMU.getAccelData(&accX, &accY, &accZ); // 加速度データ取得
+  // if (se_flag)
+  // {
+  //   mp3->begin(id3, out);
+  //   se_flag = false;
+  // }
+
+  // if (mp3->isRunning())
+  // {
+  //   if (!mp3->loop())
+  //     mp3->stop();
+  // }
+  // else
+  // {
+  //   Serial.printf("MP3 done\n");
+  // }
+  size_t bytes_written;
+  i2s_write(SPEAKER_I2S_NUMBER, wavList[0], wavSize[0], &bytes_written, portMAX_DELAY);
+  i2s_zero_dma_buffer(SPEAKER_I2S_NUMBER);
+
+}
+// ---------------------------------------------------------------
+void InitI2SSpeakerOrMic(int mode)
+{
+  esp_err_t err = ESP_OK;
+  i2s_driver_uninstall(SPEAKER_I2S_NUMBER);
+  i2s_config_t i2s_config = {
+    .mode                 = (i2s_mode_t)(I2S_MODE_MASTER),
+    .sample_rate          = 16000,
+    .bits_per_sample      = I2S_BITS_PER_SAMPLE_16BIT,
+    .channel_format       = I2S_CHANNEL_FMT_ALL_RIGHT,
+    .communication_format = I2S_COMM_FORMAT_I2S,
+    .intr_alloc_flags     = ESP_INTR_FLAG_LEVEL1,
+    .dma_buf_count        = 6,
+    .dma_buf_len          = 60,
+    .use_apll             = false,
+    .tx_desc_auto_clear   = true,
+    .fixed_mclk           = 0
+  };
+  if (mode == MODE_MIC) {
+    i2s_config.mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_PDM);
+  } else {
+    i2s_config.mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX);
+  }
+  err += i2s_driver_install(SPEAKER_I2S_NUMBER, &i2s_config, 0, NULL);
+  i2s_pin_config_t tx_pin_config = {
+    .bck_io_num           = CONFIG_I2S_BCK_PIN,
+    .ws_io_num            = CONFIG_I2S_LRCK_PIN,
+    .data_out_num         = CONFIG_I2S_DATA_PIN,
+    .data_in_num          = CONFIG_I2S_DATA_IN_PIN,
+  };
+  err += i2s_set_pin(SPEAKER_I2S_NUMBER, &tx_pin_config);
+  if (mode != MODE_MIC) {
+    err += i2s_set_clk(SPEAKER_I2S_NUMBER, 16000, I2S_BITS_PER_SAMPLE_16BIT, I2S_CHANNEL_MONO);
+  }
+  i2s_zero_dma_buffer(SPEAKER_I2S_NUMBER);
 }
 // ---------------------------------------------------------------
